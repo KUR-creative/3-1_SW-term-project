984bf4c4153a6cc337103ae422a3602b2efc7451
KUR <rhdnfka94@gmail.com>
2017-06-02 11:49:37 +0900

    add Saver and DBHelper

---
+++ app/src/main/java/com/example/kouram/usetmap/DBHelper.java
@@ -0,0 +1,8 @@
+package com.example.kouram.usetmap;
+
+/**
+ * Created by Ko U Ram on 2017-06-02.
+ */
+
+class DBHelper {
+}
---
+++ app/src/main/java/com/example/kouram/usetmap/Saver.java
@@ -0,0 +1,14 @@
+package com.example.kouram.usetmap;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Ko U Ram on 2017-06-02.
+ */
+
+class Saver {
+    SQLiteDatabase db;
+    DBHelper helper;
+
+
+}
